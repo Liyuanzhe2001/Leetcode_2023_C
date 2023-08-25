74f68649fceb4a4388ad95801307ca0c31701d48
Liyuanzhe2001 <619356147@qq.com>
2023-08-25 21:20:35 +0800

    update

--- main/数据结构教程/线性表/单链表及其应用/lc_反转链表 II.cpp
+++ main/数据结构教程/线性表/单链表及其应用/lc_反转链表 II.cpp
@@ -21,7 +21,37 @@ using namespace std;
 
 class Solution {
 public:
-    ListNode *reverseBetween(ListNode *head, int left, int right) {
+    ListNode *reverseList(ListNode *head) {
+        ListNode *pre = nullptr;
+        ListNode *curr = head;
+        while (curr) {
+            ListNode *tmp = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = tmp;
+        }
+        return pre;
+    }
 
+    ListNode *reverseBetween(ListNode *head, int left, int right) {
+        if (left != 1) {
+            head->next = reverseBetween(head->next, left - 1, right - 1);
+            return head;
+        }
+
+        ListNode* root = head;
+        for (int i = 1; i < right; i++) {
+            head = head->next;
+        }
+
+        ListNode *tmp = head->next;
+        head->next = nullptr;
+        ListNode *rootR = reverseList(root);
+        ListNode *headPart = rootR;
+        while (headPart->next) {
+            headPart = headPart->next;
+        }
+        headPart->next = tmp;
+        return rootR;
     }
 };
---
+++ main/数据结构教程/线性表/单链表及其应用/lc_奇偶链表.cpp
@@ -0,0 +1,38 @@
+//
+// Created by LiYuanzhe on 2023/8/25.
+//
+
+#include "iostream"
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode *oddEvenList(ListNode *head) {
+        if (!head) {
+            return nullptr;
+        }
+        ListNode *even = head->next;
+        ListNode *odd = head;
+        ListNode *evenHead = even;
+        while (even && even->next) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
