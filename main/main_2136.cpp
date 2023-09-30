7c4a3b64697b6e87186ab97590944745864e426f
Liyuanzhe2001 <619356147@qq.com>
2023-09-30 23:04:55 +0800

    update

---
+++ main/main_2136.cpp
@@ -0,0 +1,27 @@
+//
+// Created by LiYuanzhe on 2023/9/30.
+//
+
+#include "vector"
+#include "algorithm"
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+public:
+    int earliestFullBloom(vector<int> &plantTime, vector<int> &growTime) {
+        int n = plantTime.size();
+        vector<int> id(n);
+        iota(id.begin(), id.end(), 0);
+        sort(id.begin(), id.end(), [&](int i, int j) {
+            return growTime[i] > growTime[j];
+        });
+        int prev = 0, ans = 0;
+        for (int i: id) {
+            ans = max(ans, prev + plantTime[i] + growTime[i]);
+            prev += plantTime[i];
+        }
+        return ans;
+    }
+};
