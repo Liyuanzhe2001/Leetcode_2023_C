1826fa3bca29bbab9c94c8c39f71d4586c323a4d
Liyuanzhe2001 <619356147@qq.com>
2023-10-12 08:09:48 +0800

    update

--- main/数据结构教程/图/图遍历及其应用/lc_判断二分图.cpp
+++ main/数据结构教程/图/图遍历及其应用/lc_判断二分图.cpp
@@ -1,3 +1,26 @@
 //
 // Created by LiYuanzhe on 2023/10/11.
 //
+#include "vector"
+#include "string"
+
+using namespace std;
+
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int> &nums) {
+        long long res = 0;
+        int lef = 0;
+        int rig = nums.size() - 1;
+        while (lef <= rig) {
+            if (lef == rig) {
+                res += nums[lef];
+            } else {
+                res += stoi(to_string(nums[lef]) + to_string(nums[rig]));
+            }
+            lef++;
+            rig--;
+        }
+        return res;
+    }
+};
