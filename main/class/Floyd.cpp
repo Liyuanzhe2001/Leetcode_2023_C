22e3622d6380d39eba270670a12b5c28b8f752cd
Liyuanzhe2001 <619356147@qq.com>
2023-11-12 14:02:17 +0800

    update

--- main/class/Floyd.cpp
+++ main/class/Floyd.cpp
@@ -2,7 +2,39 @@
 // Created by LiYuanzhe on 2023/11/12.
 //
 #include "algorithm"
+#include "vector"
 
 using namespace std;
 
-class Floyed
+class FloydAlgorithm {
+public:
+    FloydAlgorithm(int n, vector<tuple<int, int, int>> roads) {
+        map.resize(n, vector<int>(n, INT_MAX));
+        this->len = n;
+        for (int i = 0; i < n; i++) {
+            map[i][i] = 0;
+        }
+        for (const auto &[n1, n2, len]: roads) {
+            map[n1][n2] = map[n2][n1] = len;
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    map[i][j] = min(map[i][j], map[i][k] + map[k][j]);
+                }
+            }
+        }
+    }
+
+    int getLen(int node1, int node2) {
+        if (node1 < 0 || node1 >= len || node2 < 0 || node2 >= len || map[node1][node2] == INT_MAX) {
+            return -1;
+        }
+        return map[node1][node2];
+    }
+
+private:
+    vector<vector<int>> map;
+    int len;
+
+};
--- main/main_1334.cpp
+++ main/main_1334.cpp
@@ -1,3 +1,44 @@
 //
 // Created by LiYuanzhe on 2023/11/12.
 //
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>> &edges, int distanceThreshold) {
+        vector<vector<int>> map(n, vector<int>(n, INT_MAX));
+        for (int i = 0; i < n; ++i) {
+            map[i][i] = 0;
+        }
+        for (const auto &edge: edges) {
+            map[edge[0]][edge[1]] = map[edge[1]][edge[0]] = edge[2];
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (map[i][k] != INT_MAX && map[k][j] != INT_MAX && map[i][k] + map[k][j] < map[i][j]) {
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+        int resIndex = 0;
+        int resVal = n;
+        for (int i = 0; i < n; i++) {
+            int tmp = 0;
+            for (int j = 0; j < n; j++) {
+                if (i != j && map[i][j] <= distanceThreshold) {
+                    tmp++;
+                }
+            }
+            if (tmp <= resVal) {
+                resVal = tmp;
+                resIndex = i;
+            }
+        }
+        return resIndex;
+    }
+};
