5ad43895deb5fd98e25d290f610494d6a09f9e84
Liyuanzhe2001 <619356147@qq.com>
2023-11-21 20:35:11 +0800

    update

---
+++ main/main_2216.cpp
@@ -0,0 +1,29 @@
+//
+// Created by LiYuanzhe on 2023/11/21.
+//
+#include "vector"
+#include "stack"
+
+using namespace std;
+
+class Solution {
+public:
+    int minDeletion(vector<int> &nums) {
+        int pre = -1;
+        int n = 0;
+        int s = 0;
+        for (int num: nums) {
+            if (n % 2 == 0) {
+                pre = num;
+                n++;
+            } else {
+                if (num != pre) {
+                    n++;
+                    s++;
+                }
+            }
+        }
+
+        return nums.size() - 2 * s;
+    }
+};
