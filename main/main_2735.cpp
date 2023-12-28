086b51ee434f9e6cf51d5f532107fb614f580f2c
Liyuanzhe2001 <619356147@qq.com>
2023-12-28 23:15:06 +0800

    update

---
+++ main/main_2735.cpp
@@ -0,0 +1,25 @@
+//
+// Created by LiYuanzhe on 2023/12/28.
+//
+#include "vector"
+
+using namespace std;
+
+class Solution {
+public:
+    long long minCost(vector<int> &nums, int x) {
+        int n = nums.size();
+        vector<long long> s(n); // s[k] 统计操作 k 次的总成本
+        for (int i = 0; i < n; i++) {
+            s[i] = (long long) i * x;
+        }
+        for (int i = 0; i < n; i++) { // 子数组左端点
+            int mn = nums[i];
+            for (int j = i; j < n + i; j++) { // 子数组右端点（把数组视作环形的）
+                mn = min(mn, nums[j % n]); // 维护从 nums[i] 到 nums[j] 的最小值
+                s[j - i] += mn; // 累加操作 j-i 次的花费
+            }
+        }
+        return *min_element(s.begin(), s.end());
+    }
+};
